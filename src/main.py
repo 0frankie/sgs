1f90236d6ba0491818b6f7cb41138ecee187b497
FwankiL <89987389+0frankie@users.noreply.github.com>
2025-04-06 16:44:25 -0400

    basic bot done

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("Hello world!")
+import os
+
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv("DISCORD_TOKEN"))
